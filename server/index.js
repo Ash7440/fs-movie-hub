d3552c7930066356b9a4f5976199feca00a97e56
Ash7440 <ash.pship@gmail.com>
2026-03-29 17:15:59 +0300

    add video streaming

--- server/index.js
+++ server/index.js
@@ -25,6 +25,51 @@ app.get('/api/movies', (req, res) => {
   })
 })
 
+app.get('/api/video/:filename', (req, res) => {
+  const fileName = req.params.filename
+  const videoPath = path.join(MOVIES_DIR, fileName)
+
+  if (!fs.existsSync(videoPath)) {
+    return res.status(404).send('film not found')
+  }
+
+  const stat = fs.statSync(videoPath)
+  const fileSize = stat.size
+  const range = req.headers.range
+
+  if (range) {
+    const parts = range.replace(/bytes=/, '').split('-')
+    const start = parseInt(parts[0], 10)
+    const end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1
+
+    if (start >= fileSize) {
+      res.status(406).send('value is out of range')
+      return
+    }
+
+    const chunksize = (end - start) + 1
+    const file = fs.createReadStream(videoPath, { start, end })
+
+    const head = {
+      'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+      'Accept-Ranges': 'bytes',
+      'Content-Length': chunksize,
+      'Content-Type': 'video/mp4',
+    }
+
+    res.writeHead(206, head)
+    file.pipe(res)
+  } else {
+    const head = {
+      'Content-Length': fileSize,
+      'Content-Type': 'video/mp4',
+      'Accept-Ranges': 'bytes',
+    }
+    res.writeHead(200, head)
+    fs.createReadStream(videoPath).pipe(res)
+  }
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`server run on port: ${PORT}`)
