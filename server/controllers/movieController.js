34c89fadb0ecf5d685903ec23050468f6bfc1604
Ash7440 <ash.pship@gmail.com>
2026-03-31 17:54:54 +0300

    add try/catch to stream video

--- server/controllers/movieController.js
+++ server/controllers/movieController.js
@@ -62,47 +62,52 @@ const getMovies = async (req, res) => {
 }
 
 const streamVideo = (req, res) => {
-  const fileName = req.params.filename
-  const videoPath = path.join(moviesDir, fileName)
-
-  if (!fsSync.existsSync(videoPath)) {
-    return res.status(404).send('film not found')
-  }
-
-  const stat = fsSync.statSync(videoPath)
-  const fileSize = stat.size
-  const range = req.headers.range
-
-  if (range) {
-    const parts = range.replace(/bytes=/, '').split('-')
-    const start = parseInt(parts[0], 10)
-    const end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1
-
-    if (start >= fileSize) {
-      res.status(406).send('value is out of range')
-      return
+  try {
+    const fileName = req.params.filename
+    const videoPath = path.join(moviesDir, fileName)
+    
+    if (!fsSync.existsSync(videoPath)) {
+      return res.status(404).send('film not found')
     }
+    
+    const stat = fsSync.statSync(videoPath)
+    const fileSize = stat.size
+    const range = req.headers.range
+    
+    if (range) {
+      const parts = range.replace(/bytes=/, '').split('-')
+      const start = parseInt(parts[0], 10)
+      const end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1
 
-    const chunksize = (end - start) + 1
-    const file = fsSync.createReadStream(videoPath, { start, end })
+      if (start >= fileSize) {
+        res.status(406).send('value is out of range')
+        return
+      }
 
-    const head = {
-      'Content-Range': `bytes ${start}-${end}/${fileSize}`,
-      'Accept-Ranges': 'bytes',
-      'Content-Length': chunksize,
-      'Content-Type': 'video/mp4',
+      const chunksize = (end - start) + 1
+      const file = fsSync.createReadStream(videoPath, { start, end })
+
+      const head = {
+        'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+        'Accept-Ranges': 'bytes',
+        'Content-Length': chunksize,
+        'Content-Type': 'video/mp4',
+      }
+
+      res.writeHead(206, head)
+      file.pipe(res)
+    } else {
+      const head = {
+        'Content-Length': fileSize,
+        'Content-Type': 'video/mp4',
+        'Accept-Ranges': 'bytes',
+      }
+      res.writeHead(200, head)
+      fsSync.createReadStream(videoPath).pipe(res)
     }
-
-    res.writeHead(206, head)
-    file.pipe(res)
-  } else {
-    const head = {
-      'Content-Length': fileSize,
-      'Content-Type': 'video/mp4',
-      'Accept-Ranges': 'bytes',
-    }
-    res.writeHead(200, head)
-    fsSync.createReadStream(videoPath).pipe(res)
+  } catch (err) {
+    console.error('Global Error:', err)
+    res.status(500).json({ error: 'Internal Server Error' })
   }
 }
 
