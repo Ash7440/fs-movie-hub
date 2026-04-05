9d08c2a132eea47591e9cfe0a609ed723cccf9bd
Ash7440 <ash.pship@gmail.com>
2026-04-05 19:45:31 +0300

    feat: support client-side routing in production

--- server/index.js
+++ server/index.js
@@ -26,6 +26,10 @@ require('./utils/converter')
 
 app.use(express.static(path.join(__dirname, 'dist')))
 
+app.get(/.*/, (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+})
+
 app.listen(process.env.PORT, '0.0.0.0', () => {
   console.log(`server run on port: ${process.env.PORT}`)
 })
