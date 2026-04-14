076c7b7defb59b897eff9d022f6dd3919fa4cf0d
Ash7440 <ash.pship@gmail.com>
2026-04-14 13:43:00 +0300

    feat: wrapped <App /> in MovieProvider

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter as Router} from 'react-router-dom'
 import App from './App.jsx'
+import { MovieProvider } from './context/MovieContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <Router>
-    <App />
+    <MovieProvider>
+      <App />
+    </MovieProvider>
   </Router>,
 )
