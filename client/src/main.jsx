44136adf6144d353f48a170ae8720d71e54136d0
Ash7440 <ash.pship@gmail.com>
2026-05-26 12:46:12 +0300

    feat: implement storage cleanup by current_srorage_version

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,14 @@ import { BrowserRouter as Router} from 'react-router-dom'
 import App from './App.jsx'
 import { MovieProvider } from './context/MovieContext.jsx'
 
+const CURRENT_STORAGE_VERSION = 'v2_tokens'
+
+if (localStorage.getItem('storage_version') !== CURRENT_STORAGE_VERSION) {
+  localStorage.removeItem('cinema_user') 
+
+  localStorage.setItem('storage_version', CURRENT_STORAGE_VERSION)
+}
+
 createRoot(document.getElementById('root')).render(
   <Router>
     <MovieProvider>
