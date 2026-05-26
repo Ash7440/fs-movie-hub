eecd5d257bcae43d9dc488ca729c0fe44ee33ce7
Ash7440 <ash.pship@gmail.com>
2026-05-27 00:09:08 +0300

    fix: made playback service use token

--- client/services/playback.js
+++ client/services/playback.js
@@ -1,23 +1,34 @@
-export const getPlayback = async (baseUrl, userId, movieId) => {
-  const response = await fetch(`${baseUrl}/api/playback/${userId}/${movieId}`)
+export const getPlayback = async (baseUrl, token, movieId) => {
+  const response = await fetch(`${baseUrl}/api/playback/${movieId}`,{
+    method: 'GET',
+    headers: {
+      'Authorization': `Bearer ${token}`
+    }
+  })
 
   if (!response.ok) throw new Error('Failed to get playback')
 
   return await response.json()
 }
 
-export const getAllUserPlaybacks = async (baseUrl, userId) => {
-  const response = await fetch(`${baseUrl}/api/playback/user/${userId}`)
+export const getAllUserPlaybacks = async (baseUrl, token) => {
+  const response = await fetch(`${baseUrl}/api/playback/user`, {
+    method: 'GET',
+    headers: {
+      'Authorization': `Bearer ${token}`
+    }
+  })
 
   if (!response.ok) throw new Error('Failed to get all playbacks')
 
   return await response.json()
 }
 
-export const postPlayback = async (baseUrl, data) => {
+export const postPlayback = async (baseUrl, token, data) => {
   const response = await fetch(`${baseUrl}/api/playback`, {
     method: 'POST',
     headers: {
+      'Authorization': `Bearer ${token}`,
       'Content-Type': 'application/json'
     },
     body: JSON.stringify(data)
